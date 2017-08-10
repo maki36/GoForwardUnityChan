9612c872af39f413d8e268f5ac3fb5e694c678ce
maki <tubou30@yahoo.co.jp>
2017-08-10 21:15:22 +0900

    8月10日の確認データです。森田
    エラーの原因がわからないため、一旦提出です。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+	//private float groundLevel = -3.0f;
+
+
+	// Use this for initialization
+	void Start(){
+	}
+		
+	void OnCollisonEnter2D(Collision2D other){
+		Debug.Log (other.gameObject.tag);
+
+		if (gameObject.tag == "Cube") {
+			GetComponent<AudioSource>().Play();
+
+			//audio.Play();
+			//GetComponent<AudioSource> ();
+		}
+		//if (other.gameObject.tag == "Ground") {
+		//	GetComponent<AudioSource>();
+
+			//audio.Play();
+			//GetComponent<AudioSource> ();
+		//}
+		//if (other.gameObject.tag != "Unitytyan") {
+		//	GetComponent<AudioSource> ().volume = 0;
+		//}
+		//if (other.gameObject.tag != "Ground") {
+		//	GetComponent<AudioSource> ().volume = 0;
+		//}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		// キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine){
+			Destroy (gameObject);
+		}
+	}
+}
